93ebd41d82fef0c89b82568a2824ad14dc8e66b7
Rodrigo <rodrigo_salgado_@hotmail.com>
2021-12-12 19:55:19 -0300

    word promotion changed to sale

--- back-end/controllers/sales.js
+++ back-end/controllers/sales.js
@@ -1,12 +1,12 @@
 const Promotion = require('../models/sales')
 
 module.exports = app => {
-    app.get('/sales', (req, res) => {
+    app.get('/api/sales', (req, res) => {
         Promotion.get()
             .then(data => res.json(data))
             .catch(errors => res.status(400).json(errors))
     })
-    app.get('/sales/:id', (req, res) => {
+    app.get('/api/sales/:id', (req, res) => {
         const id = parseInt(req.params.id)
 
         Promotion.getById(id)
@@ -14,7 +14,7 @@ module.exports = app => {
             .catch(errors => res.status(400).json(errors))
     })
 
-    app.post('/sales', (req, res) => {
+    app.post('/api/sales', (req, res) => {
         const promotion = req.body
 
         Promotion.add(promotion)
@@ -26,7 +26,7 @@ module.exports = app => {
             )
     })
 
-    app.patch('/sales/:id', (req, res) => {
+    app.patch('/api/sales/:id', (req, res) => {
         const id = parseInt(req.params.id)
         const promotion = req.body
 
@@ -37,7 +37,7 @@ module.exports = app => {
             .catch(errors => res.status(400).json(errors))
     })
 
-    app.delete('/sales/:id', (req, res) => {
+    app.delete('/api/sales/:id', (req, res) => {
         const id = parseInt(req.params.id)
 
         Promotion.delete(id)
